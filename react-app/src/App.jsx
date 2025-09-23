66d704038eb10ff62efd58b06294d37a64f1726b
Kuzcobain <samuelpro9876@gmail.com>
2025-09-23 04:18:18 -0500

    Front de la página Inicio (actualización posterior)

---
+++ react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Banner from "./components/Banner/Banner";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
