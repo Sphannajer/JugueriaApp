1809f76f7d4cc65fb45097e0cdf7307e6b046daf
gabrielyg2 <juliogabrielyg2@gmail.com>
2025-09-25 02:38:17 -0500

    Edicion de la App para correcto funcionamiento

--- Frontend/JugueriaFront/src/App.jsx
+++ Frontend/JugueriaFront/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Login from './pages/Login.jsx'
 import Register from "./pages/Register.jsx"
 import Visitanos from "./pages/Visitanos.jsx";
+import Contactanos from "./pages/Contactanos.jsx";
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/visitanos" element={<Visitanos />}/>
+      <Route path="/contactanos" element={<Contactanos />} />
     </Routes>
     
   );
