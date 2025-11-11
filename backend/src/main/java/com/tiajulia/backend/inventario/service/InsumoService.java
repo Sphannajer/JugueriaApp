8fd49fd5deaa0a508b2f4fe26db0d97ab30da989
Sphannajer Fuentes <sphannajerfuentes@gmail.com>
2025-11-10 23:00:09 -0500

    Backend Subido Para inventario

--- backend/src/main/java/com/tiajulia/backend/inventario/service/InsumoService.java
+++ backend/src/main/java/com/tiajulia/backend/inventario/service/InsumoService.java
@@ -1,4 +1,14 @@
 package com.tiajulia.backend.inventario.service;
 
+import com.tiajulia.backend.inventario.model.Insumo;
+
+import java.util.List;
+
 public interface InsumoService {
+    List<Insumo> listarTodos();
+    Insumo guardar(Insumo insumo);
+    Insumo actualizar(Integer id, Insumo insumo);
+    void eliminar(Integer id);
+    List<Object[]> obtenerStockPorInsumo();
+    Double obtenerValorTotalInventario();
 }
